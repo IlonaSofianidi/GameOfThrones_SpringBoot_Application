24ab6f290cc21fe97e1b7e0196402514a13a7e97
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-26 11:50:50 +0200

    Init commit.

---
+++ src/main/java/com/example/SpringBoot_GameOfThrones/bootloader/SpringBootGameOfThronesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_GameOfThrones.bootloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGameOfThronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGameOfThronesApplication.class, args);
+	}
+
+}
