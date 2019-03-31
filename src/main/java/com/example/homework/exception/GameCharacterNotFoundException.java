9ef66d841d1e33dac4e1b96d8607754f35276de6
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-31 19:45:03 +0300

    Add custom exceptions.

---
+++ src/main/java/com/example/homework/exception/GameCharacterNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.homework.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class GameCharacterNotFoundException extends  RuntimeException{
+    public GameCharacterNotFoundException(String ex){
+        super(ex);
+    }
+}
