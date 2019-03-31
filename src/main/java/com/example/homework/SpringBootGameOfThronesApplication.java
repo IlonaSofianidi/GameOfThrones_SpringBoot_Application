3bff285ae39f0f525d15fc97afca08f829ec01c9
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-31 19:43:04 +0300

    Rename package SpringBoot_GameOfThrones to homework.

--- src/main/java/com/example/homework/SpringBootGameOfThronesApplication.java
+++ src/main/java/com/example/homework/SpringBootGameOfThronesApplication.java
@@ -1,11 +1,10 @@
-package com.example.SpringBoot_GameOfThrones.bootloader;
-
+package com.example.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SpringBootGameOfThronesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGameOfThronesApplication.class, args);
 	}
