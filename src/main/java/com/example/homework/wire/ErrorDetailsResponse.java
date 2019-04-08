461e7959882597e12a94fd1aae8516183b5f1c24
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-04-08 23:06:24 +0300

    Add getter to ErrorDetailsResponse.

--- src/main/java/com/example/homework/wire/ErrorDetailsResponse.java
+++ src/main/java/com/example/homework/wire/ErrorDetailsResponse.java
@@ -1,10 +1,12 @@
 package com.example.homework.wire;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@Getter
 public class ErrorDetailsResponse {
     private Date timestamp;
     private String message;
